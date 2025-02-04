10d9f3fd1a2d87df638b0f6fe0a58f2e9226ebb2
David Ksendzulak <david.ksendzulak@webology.com>
2025-02-04 18:40:50 +0100

    project setup

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ function App() {
     <>
       <h1>My project for PRAX</h1>
       <p>Teacher: Julia Dlugosova</p>
-      <img src="https://i.pinimg.com/736x/b2/95/98/b29598e038ab9fc75aac59cfb9e9dc8b.jpg"/>
     </>
   )
 }
